d00962205955839caf8b125001a8a734848998de
Alan Orth <alan.orth@gmail.com>
2020-05-06 16:35:39 +0300

    auxiliaries/pipeline_auxiliaries.py: Fix call to time()
    When the output directory already exists for a previous phase that
    has not completed, the pipeline dies with this error when creating
    a new, time-stamped command path:
    
      File "./auxiliaries/pipeline_auxiliaries.py", line 180, in
    submit_pipeline_step_to_cluster
        cmds_path = os.path.join(tmp_dir, f'{job_name}_{time()}.cmds')
    TypeError: 'module' object is not callable
    
    We need to call time.time() instead of time().

--- auxiliaries/pipeline_auxiliaries.py
+++ auxiliaries/pipeline_auxiliaries.py
@@ -180,7 +180,7 @@ def submit_pipeline_step_to_cluster(script_path, params_lists, tmp_dir, job_name
     cmds_as_str += '\t' + job_name + '\n'
     cmds_path = os.path.join(tmp_dir, f'{job_name}.cmds')
     if os.path.exists(cmds_path):
-        cmds_path = os.path.join(tmp_dir, f'{job_name}_{time()}.cmds')
+        cmds_path = os.path.join(tmp_dir, f'{job_name}_{time.time()}.cmds')
     with open(cmds_path, 'w') as f:
         f.write(cmds_as_str)
 
@@ -254,17 +254,12 @@ def run_step_locally(script_path, params_lists, tmp_dir, job_name, queue_name, v
     return example_cmd
 
 def submit_pipeline_step(script_path, params_lists, tmp_dir, job_name, queue_name, verbose, new_line_delimiter='!@#',
-<<<<<<< HEAD
-                         q_submitter_script_path='/bioseq/bioSequence_scripts_and_constants/q_submitter_power.py',
+                         q_submitter_script_path=global_params.qsub_script,
                          required_modules_as_list=None, num_of_cpus=1, executable='python', done_path = None):
     if done_path and os.path.exists(done_path):
         process, example_cmd = create_command(**locals())
         logger.info(f'Skipping "{example_cmd}" as "{done_path}" exists')
         return example_cmd
-=======
-                         q_submitter_script_path=global_params.qsub_script,
-                         required_modules_as_list=None, num_of_cpus=1):
->>>>>>> a43dc68... auxiliaries/pipeline_auxiliaries.py: Get qsub_script from global params
     if global_params.run_using_celery:
         return submit_pipeline_step_to_celery(**locals())
     if global_params.is_run_on_cluster:
